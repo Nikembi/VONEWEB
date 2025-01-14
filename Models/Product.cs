d58084e87d5a77883365d886a0c2bec1ffb89949
Nikembi Travis <nikembitravis@outlook.com>
2025-01-14 05:29:32 -0600

    Started models, and SQL Config

--- Models/Product.cs
+++ Models/Product.cs
@@ -1,6 +1,15 @@
-﻿namespace VONEWEB.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VONEWEB.Models
 {
     public class Product
     {
+        public int ProductId { get; set; }
+        public string Description { get; set; }
+
+        [MaxLength(255)]
+        public string ImageUrl { get; set; }
+        public decimal Price { get; set; }
+        public decimal CostPrice { get; set; }
     }
 }
--- Models/User.cs
+++ Models/User.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+using System.Security.Cryptography;
+using VONEWEB.Enums;
 
 namespace VONEWEB.Models
 {
@@ -6,8 +9,28 @@ namespace VONEWEB.Models
     {
         public User () 
         {
-
+            ProfilePictureUrl = string.Empty;
+            Cart = new List<Cart>();
+            Transaction = new List<Transaction>();
+            OrderHistory = new List<OrderHistory>();
         }
 
+        [Required]
+        [MaxLength(20)]
+        public UserRole Role { get; set; } // Possible values: "backer", "creator", "admin"
+
+        [Url]
+        public string ProfilePictureUrl { get; set; }
+
+        [MaxLength(500)]
+        
+        public DateTime DateJoined { get; set; } = DateTime.UtcNow;
+
+        public bool IsActive { get; set; } = true;
+
+        // Navigation properties
+        public ICollection<Cart> Cart { get; set; }
+        public ICollection<Transaction> Transaction { get; set; }
+        public ICollection<OrderHistory> OrderHistory { get; set; }
     }
 }
