60c72ac184214819aa1f7a6c3674dcb59d23fe35
Nikembi Travis <nikembitravis@outlook.com>
2025-01-15 20:28:27 -0600

    Had to make changes to model and program, this is before first migration

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Configuration.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
 
 builder.Services.AddDbContext<VONEDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("fundingDB")));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DB")));
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
