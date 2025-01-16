b57d0399828df26b0034375387022c329aeb2f4e
Nikembi Travis <nikembitravis@outlook.com>
2025-01-15 20:12:39 -0600

    Added model tables in dbcontext, fixed all posing errors..

--- Data/VONEDbContext.cs
+++ Data/VONEDbContext.cs
@@ -1,11 +1,19 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using VONEWEB.Models;
 
 namespace VONEWEB.Data
 {
-    public class VONEDbContext : IdentityDbContext
+    public class VONEDbContext : IdentityDbContext<User, IdentityRole<Guid>, Guid>
     {
-
+        DbSet<Customer> customers { get; set; }
+        DbSet<Order> orders { get; set; }
+        DbSet<Product> products { get; set; }
+        DbSet<OrderItem> ordersItems { get; set; }
+        DbSet<Transaction> transactions { get; set; }
+        DbSet<CartItem> cartTrans { get; set; }
+        
         
         public VONEDbContext(DbContextOptions<VONEDbContext> options) 
             : base(options) 
